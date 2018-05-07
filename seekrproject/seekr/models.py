81cff202893bd3a9c77a7d94a380ed7a48da4e02
Minhquan Thai <minhquanthai@gmail.com>
2018-05-07 15:38:31 -0400

    Made company and job models

--- seekrproject/seekr/models.py
+++ seekrproject/seekr/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    industry = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    url = models.CharField(max_length=100)
+    glassdoor_link = models.TextField()
+    
+class Job(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='jobs')
+    title = models.CharField(max_length=100)
+    requirements = models.TextField()
+    salary_range_start = models.PositiveIntegerField()
+    salary_range_end = models.PositiveIntegerField()
+    source = models.TextField()
+    date_posted = models.DateField()
