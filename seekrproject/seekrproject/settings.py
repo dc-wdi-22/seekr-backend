4b9beabea4174d0a15c119cd986da0e1bcdcc4bf
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-05-07 15:09:55 -0400

    templates

--- seekrproject/seekrproject/settings.py
+++ seekrproject/seekrproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'seekr',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'seekrproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'seekr',
+        'USER': 'seekruser',
+        'PASSWORD': 'seekr1',
+        'HOST': 'localhost'
     }
 }
 
