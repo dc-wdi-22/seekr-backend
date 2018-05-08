8dd5b371756ee6a7a9763105484fa7b927e13ce8
Matthew Sharpe <mattsharpe72@gmail.com>
2018-05-08 11:09:20 -0400

    urls working

--- seekrproject/seekr/urls.py
+++ seekrproject/seekr/urls.py
@@ -7,9 +7,9 @@ from . import views
 urlpatterns = [
     path('companies', views.CompanyList.as_view(), name='company-list'),
     path('jobs', views.JobList.as_view(), name='job-list'),
-    path('company/<int:id>', views.CompanyDetail.as_view(), name='company-detail'),
-    path('job/<int:id>', views.JobDetail.as_view(), name='job-detail'),
-    path('todos/<int:id>', views.TodoItemDetail.as_view(), name='todo-detail')
+    path('company/<int:pk>', views.CompanyDetail.as_view(), name='company-detail'),
+    path('job/<int:pk>', views.JobDetail.as_view(), name='job-detail'),
+    path('todos/<int:pk>', views.TodoItemDetail.as_view(), name='todo-detail')
 ]
 
 # will write other urls when model is updated and migrated correctly
