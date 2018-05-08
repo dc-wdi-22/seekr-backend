c8c94cd84029171e85a3697f3b228593b6034a94
Matthew Sharpe <mattsharpe72@gmail.com>
2018-05-08 10:47:18 -0400

    working on bugs

--- seekrproject/seekr/urls.py
+++ seekrproject/seekr/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
 urlpatterns = [
-    path('companies', views.CompanyList.as_view(), name='company_list')
+    path('companies', views.CompanyList.as_view(), name='company-list'),
+    path('jobs', views.JobList.as_view(), name='job-list'),
+    path('company/<int:id>', views.CompanyDetail.as_view(), name='company-detail'),
+    path('job/<int:id>', views.JobDetail.as_view(), name='job-detail'),
+    path('todos/<int:id>', views.TodoItemDetail.as_view(), name='todo-detail')
 ]
 
 # will write other urls when model is updated and migrated correctly
