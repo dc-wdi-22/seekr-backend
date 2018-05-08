f0cf3d231ec648b44354a9b81892066915bd5354
Jason Toups <jason.p.toups@gmail.com>
2018-05-08 14:17:29 -0400

    updated serializer to render company and todo list as objects instead of as links

--- seekrproject/seekr/serializers.py
+++ seekrproject/seekr/serializers.py
@@ -11,25 +11,19 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
         model = Company
         fields = ('name', 'industry', 'address', 'url', 'glassdoor_link', 'jobs',)
 
-class JobSerializer(serializers.HyperlinkedModelSerializer):
-    company = serializers.HyperlinkedRelatedField(
-        view_name = 'company-detail',
+class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
+    job = serializers.HyperlinkedRelatedField(
+        view_name = 'job-detail',
         read_only = True
     )
-    todo_list = serializers.HyperlinkedRelatedField(
-        view_name = 'todo-detail',
-        many = True,
-        read_only = True
-    )
+    class Meta:
+        model = TodoItem
+        fields = ('status', 'name', 'job')
+
+class JobSerializer(serializers.ModelSerializer):
+    company = CompanySerializer(read_only=True)
+    todo_list = TodoItemSerializer(many=True, read_only=True)
     class Meta:
         model = Job
         fields = ('company', 'title', 'description', 'requirements', 'salary_range_start', 'salary_range_end', 'source', 'notes', 'date_posted', 'todo_list', 'job_status',)
 
-class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
-    # job = serializers.HyperlinkedRelatedField(
-    #     view_name = 'job-detail',
-    #     read_only = True
-    # )
-    class Meta:
-        model = TodoItem
-        fields = ('status', 'name',)
