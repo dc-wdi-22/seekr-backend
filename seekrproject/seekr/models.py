d12ae69da7577cef7ee15dd5cfdedb2ede0a3886
Peterson Onyeukwu <code.jrgroup@gmail.com>
2018-05-08 09:08:54 -0400

    fixing the models

--- seekrproject/seekr/models.py
+++ seekrproject/seekr/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
-    industry = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    url = models.CharField(max_length=100)
-    glassdoor_link = models.TextField()
+    industry = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    url = models.CharField(max_length=100, blank=True, null=True)
+    glassdoor_link = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class TodoItem(models.Model):
-    status = models.BooleanField()
-    name = models.CharField(max_length=100)
+    status = models.NullBooleanField()
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -22,13 +22,13 @@ class TodoItem(models.Model):
     
 class Job(models.Model):
     title = models.CharField(max_length=100, blank=True)
-    description = models.CharField(max_length=1000, blank=True)
-    requirements = models.TextField(blank=True)
-    salary_range_start = models.PositiveIntegerField()
-    salary_range_end = models.PositiveIntegerField()
-    source = models.CharField(max_length=100)
-    notes = models.TextField(blank=True)
-    date_posted = models.DateField()
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    requirements = models.TextField(blank=True, null=True)
+    salary_range_start = models.PositiveIntegerField(null=True)
+    salary_range_end = models.PositiveIntegerField(null=True)
+    source = models.CharField(max_length=100, null=True)
+    notes = models.TextField(blank=True, null=True)
+    date_posted = models.DateField(null=True)
     todo_list = models.ManyToManyField(TodoItem)
     job_status = models.CharField(
         max_length = 100,
@@ -41,7 +41,8 @@ class Job(models.Model):
 
         ),
         unique = True,
-        blank=True
+        blank=True,
+        null=True
     )
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='jobs')
     def __str__(self):
