2636c1d80a2f37c33e543c02f2426dd11a6a3e90
Peterson Onyeukwu <code.jrgroup@gmail.com>
2018-05-09 10:19:38 -0400

    debugging

--- seekrproject/seekrproject/settings.py
+++ seekrproject/seekrproject/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'seekr',
-    'rest_framework',
-    'corsheaders'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
